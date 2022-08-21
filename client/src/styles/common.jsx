39a51c8cb084baf55c38c5f88c3ef26e02bad43b
whoiseon <whois__@naver.com>
2022-08-21 14:06:15 +0900

    update: global style setting and start working on web design

---
+++ client/src/styles/common.jsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const BACKGROUND_COLOR = '#121212';
+export const WHITE_COLOR = '#F5F5F5';
+export const INPUT_COLOR = '#202020';
+export const BUTTON_HOVER = '#303030';
