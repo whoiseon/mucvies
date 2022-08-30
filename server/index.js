1cf6ac767b032383cd1a768bdd64afcfc5e61fb1
whoiseon <whois__@naver.com>
2022-08-30 15:09:53 +0900

    setting: server routing

--- server/index.js
+++ server/index.js
@@ -35,10 +35,10 @@ const CLIENT_ID = process.env.NAVER_CLIENT_ID;
 const CLIENT_SECRET = process.env.NAVER_CLIENT_SECRET;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
